5b18fa710d5da0e8043c209f533c4aa849777aa5
Felipebatistapastore <125515160+Felipebatistapastore@users.noreply.github.com>
2024-03-22 21:03:11 -0300

    Add files via upload

---
+++ introphp-4cti24-main (1)/introphp-4cti24-main/1_variaveis/2_operadores_logicos/2_if_else.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 17;
+
+if($idade < 18){
+    echo 'Você não pode entrar aqui';
+} else{
+    echo 'seja bem vindo - delício';
+}
+
+
+
+
+
+?>
---
+++ introphp-4cti24-main (1)/introphp-4cti24-main/1_variaveis/2_operadores_logicos/3_logico_operacional.php
@@ -0,0 +1,12 @@
+<?php
+
+  $idade = 21;
+  $identidade = true;
+  if($idade > 18 && $identidade == true){
+   echo 'seja bem vindo';
+  } else{
+   echo 'tchau, obg! ';
+  }
+
+
+?>
